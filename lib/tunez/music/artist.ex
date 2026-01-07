05509b9f4dd592bd658b3e27b5adcd54da0a1f53
Tsathoqqua <tsathoqqua@gmail.com>
2026-01-07 20:12:37 +0100

    Documentation update

---
+++ lib/tunez/music/artist.ex
@@ -0,0 +1,20 @@
+defmodule Tunez.Music.Artist do
+  use Ash.Resource, otp_app: :tunez, domain: Tunez.Music, data_layer: AshPostgres.DataLayer
+
+  postgres do
+    table "artists"
+    repo Tunez.Repo
+  end
+
+  attributes do
+    uuid_primary_key :id
+
+    attribute :name, :string, allow_nil?: false
+    attribute :biography, :string
+
+    create_timestamp :inserted_at
+    update_timestamp :updated_at
+  end
+
+
+end
